89ed1de6396e4a519f7f88780dade59671f76ce0
dms-mrk <mariko-mamadou@outlook.fr>
2021-05-20 17:58:41 +0200

    add fixture bundle
    https://symfony.com/doc/current/bundles/DoctrineFixturesBundle/index.html

---
+++ src/DataFixtures/TaskFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TaskFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\DataFixtures;
+
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+
+    public function load(ObjectManager $manager)
+    {
+        // TODO: Implement load() method.
+    }
+}
