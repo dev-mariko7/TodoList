6d3fb3d9867ddf423303ec9cdb260b8893bea9a4
dms-mrk <mariko-mamadou@outlook.fr>
2021-05-21 10:41:27 +0200

    v1.0.2
    Add Data Fixtures Resolve

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -3,15 +3,44 @@
 
 namespace App\DataFixtures;
 
-
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Faker;
 
 class UserFixtures extends Fixture
 {
+    private $passwordencoder;
+    const NUMBER_OF_USERS = 10;
+
+    public function __construct(UserPasswordEncoderInterface $passwordencoder)
+    {
+        $this->passwordencoder = $passwordencoder;
+    }
 
     public function load(ObjectManager $manager)
     {
-        // TODO: Implement load() method.
+        $faker = Faker\Factory::create();
+        $roles = ["ROLE_USER"];
+
+        for ($i = 1; $i <= self::NUMBER_OF_USERS; ++$i) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $user->setRoles($roles);
+
+            if (1 === $i) {
+                $user->setUsername('username1');
+                $user->setPassword($this->passwordencoder->encodePassword($user, 'password1'));
+            } else {
+                $user->setUsername($faker->userName);
+                $user->setPassword($this->passwordencoder->encodePassword($user, 'pass_' . $i));
+            }
+
+            $manager->persist($user);
+            $this->addReference('User' . $i, $user);
+        }
+
+        $manager->flush();
     }
 }
