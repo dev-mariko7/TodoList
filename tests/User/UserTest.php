a8510c11ac5e113d45efbb268e2edb558b0dadb8
dms-mrk <mariko-mamadou@outlook.fr>
2021-06-24 23:56:40 +0200

    add functionnal testing add new user

--- tests/User/UserTest.php
+++ tests/User/UserTest.php
@@ -63,4 +63,47 @@ class UserTest extends WebTestCase
         //on verifie le dernier script
         $this->assertSame(1, $crawler->filter('html:contains("Vous êtes connecté en tant que username1")')->count());
     }
+
+    public function testFunctorAddUser()
+    {
+        // on se connecte
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $link = $crawler->selectLink('Se connecter')->link();
+        $crawler = $client->click($link);
+        $this->assertSame(1, $crawler->filter('html:contains("Nom d\'utilisateur")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Mot de passe")')->count());
+        $form = $crawler->selectButton('Se connecter')->form();
+        $form['username'] = 'username1';
+        $form['password'] = 'password1';
+        $client->submit($form);
+        $crawler = $client->followRedirect();
+        $this->assertSame(1, $crawler->filter('html:contains("Vous êtes connecté en tant que username1")')->count());
+
+        // == ==== on arrive sur la page d'accueil ========
+       // $client = static::createClient();
+        $link = $crawler->selectLink('Créer un utilisateur')->link();
+        $crawler = $client->click($link);
+        // ce qui doit être visible
+        $this->assertSame(1, $crawler->filter('html:contains("Nom d\'utilisateur")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Mot de passe")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Tapez le mot de passe à nouveau")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Adresse email")')->count());
+        $this->assertSame(1, $crawler->filter('html:contains("Rôle")')->count());
+        //quand on clique sur
+        $form = $crawler->selectButton('Ajouter')->form();
+        $form['user[username]'] = 'user2';
+        $form['user[password][first]'] = 'pass1';
+        $form['user[password][second]'] = 'pass1';
+        $form['user[email]'] = 'usertest@usertest.test';
+        $form['user[roles]'] = 'ROLE_USER';
+        // submit
+        $client->submit($form);
+        //on reprend la variable crawler pour récupéré le dernier script
+        //echo $client->getResponse()->getContent();
+        $crawler = $client->followRedirect();
+        //echo $client->getResponse()->getContent();
+        //on verifie le dernier script
+        $this->assertSame(1, $crawler->filter('html:contains("Superbe ! L\'utilisateur a bien été ajouté.")')->count());
+    }
 }
